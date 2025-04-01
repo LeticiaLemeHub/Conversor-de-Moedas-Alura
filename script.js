900b1f9e6a9347e69b92f749b781f9dd2b97245a
LeticiaLemeHub <devleticialeme@gmail.com>
2025-04-01 19:29:14 -0300

    Arrumei a indentação do código

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function irParaOutraPagina() {
-    window.location.href = "conversor.html";
+  window.location.href = "conversor.html";
+}
+
+function converter() {
+  let valorEmReais = document.getElementById("valor").value;
+  let taxaDeCambio = 5.68;
+
+  if (valorEmReais === "" || isNaN(valorEmReais) || valorEmReais <= 0) {
+    document.getElementById("resultado").innerText = "Por favor, digite um valor válido!";
+    return;
   }
 
-  function converter() {
-    let valorEmReais = document.getElementById("valor").value;
-    let taxaDeCambio = 5.68;
-
-    if (valorEmReais === "" || isNaN(valorEmReais) || valorEmReais <= 0) {
-        document.getElementById("resultado").innerText = "Por favor, digite um valor válido!";
-        return;
-    }
-
-    let valorEmDolares = valorEmReais / taxaDeCambio;
-    document.getElementById("resultado").innerText = `Valor em Dólares: $${valorEmDolares.toFixed(2)}`;
+  let valorEmDolares = valorEmReais / taxaDeCambio;
+  document.getElementById("resultado").innerText = `Valor em Dólares: $${valorEmDolares.toFixed(2)}`;
 }
