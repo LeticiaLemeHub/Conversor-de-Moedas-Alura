94295e413089ca50f89968e1325145c42205c9ae
LeticiaLemeHub <devleticialeme@gmail.com>
2025-04-01 17:19:21 -0300

    Adicionando arquivos HTML, CSS e JS para o conversor de moedas

---
+++ script.js
@@ -0,0 +1,16 @@
+function irParaOutraPagina() {
+    window.location.href = "conversor.html";
+  }
+
+  function converter() {
+    let valorEmReais = document.getElementById("valor").value;
+    let taxaDeCambio = 5.68;
+
+    if (valorEmReais === "" || isNaN(valorEmReais) || valorEmReais <= 0) {
+        document.getElementById("resultado").innerText = "Por favor, digite um valor válido!";
+        return;
+    }
+
+    let valorEmDolares = valorEmReais / taxaDeCambio;
+    document.getElementById("resultado").innerText = `Valor em Dólares: $${valorEmDolares.toFixed(2)}`;
+}
